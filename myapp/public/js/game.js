036fc4677ef7ecf3794f72551d7846821da47e88
MisterJuul <juliankuipers@outlook.com>
2020-01-20 16:48:34 +0100

    shared lab

--- myapp/public/js/game.js
+++ myapp/public/js/game.js
@@ -1,103 +1,68 @@
-var main = function () {
-    "use strict";
-    var currentGuesser, currrentChecker, statisticsFile, userId, guesser, gameId;
+console.log('Ive hacked the main-frame');
 
-    // TO decide roles: Every user gets a userID assigned by serever. If ID is odd, user starts as guesser and of even, starts as checker.
+$(document).ready(function(){
 
+    let selectedColor = '';
+    let guess = 0;
 
-    // Initiate userId
-    userId = 1;
+    let temp = $('.g');
+    let guessBox = [];
+    for(let i = 9; i >= 0; i--){
+        guessBox.push(temp[i]);
+    }
 
-    // Read the JSON file for the game state
-   /* statisticsFile = gameId + "-gamestatistics.json";
-    $.getJSON(statisticsFile, funtion () {
-        if (statisticsFile.currentGuesser == userId) {
-            guesser = true;
+   
+    // Need to make a submit button for the nails, and alos change the ids for the nails so that this exact button fucntion can be impmented for that one
+    $('.Button').click(function(){
+
+        $('.active').removeClass('active');
+        guess++;
+        let cur = 10 - guess;
+        for(let i = 0; i <= 4; i++){
+            $(`#${cur}-${i}`).addClass('active');
+            $(`#${cur}-${i}`).addClass('active');
         }
-        else {
-            guesser = false;
+    });
+
+
+    $('.c').click(function(){
+        let peg = ($(this).parent())[0];
+        selectedColor = 
+        $(this).css('background-color');
+        console.log(selectedColor);
+        $(peg).css('background-color', selectedColor)
+    });
+
+
+    $('.g').click(function(){
+
+        if($(this).hasClass('active')){
+            // let c = $(this).css('background-color: blue');
+            $(this).css('background-color', selectedColor)
+            // selectedColor = c;
         }
-    }); */
+    })
 
+    let clickcount = 0;
+    let c = 'red';
+    $('.n').click(function(){
 
-    // Generate the HTML for the current game state
-
-    // Let the guesser make his move and check the validity
-
-    var activeRow = 10; // this needs to eventually get the value from gamestate file
-
-    if (activeRow == 10) {
-        $("div#" + activeRow + ".GuessColor.flex").addClass("activeRow");
-    }
-    else if (activeRow < 10 && activeRow > 0) {
-        $("div#" + (activeRow + 1) + ".GuessColor.flex").removeClass("activeRow");
-        $("div#" + activeRow + ".GuessColor.flex").addClass("activeRow");
-    }
-    else {
-        // game ends
-    }
-
-    var currentColor;
-
-    $(".c.blue").on("click", function () {
-        currentColor = "blue";
-    });
-
-    $(".c.red").on("click", function () {
-        currentColor = "red";
-    });
-
-    $(".c.white").on("click", function () {
-        currentColor = "white";
-    });
-
-    $(".c.black").on("click", function () {
-        currentColor = "black";
-    });
-
-    $(".c.green").on("click", function () {
-        currentColor = "green";
-    });
-
-    $(".c.orange").on("click", function () {
-        currentColor = "orange";
-    });
-
-    $(".c.yellow").on("click", function () {
-        currentColor = "yellow";
-    });
-
-    $(".c.purple").on("click", function () {
-        currentColor = "purple";
-    });
-
-    $("div#" + activeRow + "-1").on("click", function () {
-        $("div#" + activeRow + "-1").removeClass();
-        $("div#" + activeRow + "-1").addClass("g flex " + currentColor);
-    });
-
-    $("div#" + activeRow + "-2").on("click", function () {
-        $("div#" + activeRow + "-2").removeClass();
-        $("div#" + activeRow + "-2").addClass("g flex " + currentColor);
-    });
-
-    $("div#" + activeRow + "-3").on("click", function () {
-        $("div#" + activeRow + "-3").removeClass();
-        $("div#" + activeRow + "-3").addClass("g flex " + currentColor);
-    });
-
-    $("div#" + activeRow + "-4").on("click", function () {
-        $("div#" + activeRow + "-4").removeClass();
-        $("div#" + activeRow + "-4").addClass("g flex " + currentColor);
-    });
-
-    // Modify the JSON file and send to other player
-
-    // Let the checker make his moves
-
-    // Switch the roles of the player
-
-    // Update JSON file with game statistics
-}
-
-$(document).ready(main);
+        if($(this).hasClass('active')){
+            if(clickcount == 0){
+                c = 'red';
+            }
+            if(clickcount == 1){
+                c = 'black';
+            }
+            if(clickcount == 2){
+                c = 'white';
+            }
+            clickcount++;
+            if(clickcount >= 3){
+                clickcount = 0;
+            }
+            $(this).css('background-color', c)
+        }
+        
+    })
+});
